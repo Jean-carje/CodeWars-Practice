51723af878e215653fb09f10685daa6e11fe3c84
Jean Estevez <ctarriba9@gmail.com>
2020-10-11 13:23:54 -0400

    Codewars

--- javascript/7_kyu/Maximum_Length_Difference.js
+++ javascript/7_kyu/Maximum_Length_Difference.js
@@ -4,7 +4,7 @@
 // // -------------------------------------
 // Instructions:
 /*
- *
+
 You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z.
 Let x be any string in the first array and y be any string in the second array.
 
@@ -13,21 +13,21 @@ Find max(abs(length(x) − length(y)))
 If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
 
 Example:
-	a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
-	a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
-	mxdiflg(a1, a2) --> 13
+  a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+  a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+  mxdiflg(a1, a2) --> 13
 
 */
 // -------------------------------------
 // Solution 1:
 
 function mxdiflg(a1, a2) {
-	let result = Math.max(...a1.map(x => {
-		return Math.max.apply(null, a2.map(y => {
-			return Math.abs(x.length - y.length);
-		}));
-	}));
-	return result > 0 ? result : -1;
+  let result = Math.max(...a1.map(x => {
+    return Math.max.apply(null, a2.map(y => {
+      return Math.abs(x.length - y.length);
+    }));
+  }));
+  return result > 0 ? result : -1;
 }
 
 
